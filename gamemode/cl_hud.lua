3653c9abfc5cd4af19d32c3b411daec120a8379a
cletts97 <c.letts97@outlook.com>
2017-06-24 02:05:45 +0100

    Took away the squares

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -28,14 +28,12 @@ local HideElements = {
 }
 
 hook.Add("HUDPaint","FixCHudAmmo", function()
-	draw.RoundedBox( 4, 20, 20, 512, 512, Color(150, 10, 255))
-	draw.RoundedBox( 4, 700, 700, 200, 200, Color(5, 130, 130))
 	if HudTheme:GetInt() == 2 then
 		HideElements["CHudAmmo"] = true
 	else
 		HideElements["CHudAmmo"] = false
 	end
-	--hook.Remove("HUDPaint", "FixCHudAmmo")
+	hook.Remove("HUDPaint", "FixCHudAmmo")
 end)
 
 local hudThemeCache = HudTheme:GetInt()
