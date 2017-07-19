8a97bec5d3f8136f4cc98e9718f06395e5d73683
Jad Own U <bretttaylor1998@live.co.uk>
2017-07-19 23:45:42 +0100

    Automatic veteran rank swapped to numbers. No idea how it was working with strings. LUA MAGIC

--- gamemode/Playtime/sv_giveVeteran.lua
+++ gamemode/Playtime/sv_giveVeteran.lua
@@ -3,11 +3,11 @@ PlaytimeServer = PlaytimeServer or {};
 function PlaytimeServer.CheckIfVeteran(ply, setting, newValue)
     // 30 hours. 60 minutes in an hour. 60*30
     if (setting == PlayerSettings.Enums.PLAY_TIME.Name) then
-        if (newValue == "1800") then
+        if (tonumber(newValue) == 1800) then
             Notify.SendNotificationAll(ply:Nick().." became a Veteran!", 10);
         end
 
-        if (newValue >= "1800") then
+        if (tonumber(newValue) >= 1800) then
             if (ply:IsModerator() == false && ply:IsDonator() == false && ply:GetSetting(PlayerSettings.Enums.TAG_NAME.Name) == "NIL") then
                 ply:SetSetting(PlayerSettings.Enums.TAG_NAME.Name, "Veteran");
                 ply:SetSetting(PlayerSettings.Enums.TAG_COLOUR.Name, "66-217-244");
